7326dc57dfaded4b6af2ef4c35e07a4c6b192862
DongSeok Kang <102869426+ehdtjr@users.noreply.github.com>
2024-01-23 18:35:30 +0900

    [Silver III] Title: 대한민국을 지키는 가장 긴 힘, Time: 124 ms, Memory: 110512 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/31263. 대한민국을 지키는 가장 긴 힘/대한민국을 지키는 가장 긴 힘.py
+++ 백준/Silver/31263. 대한민국을 지키는 가장 긴 힘/대한민국을 지키는 가장 긴 힘.py
@@ -4,19 +4,17 @@ S=input()
 count=0
 i=2
 
-while S:
-    if len(S)>3:
-        i=2
-        while S[i+1]=='0':
-            i-=1
-        S=S[i+1:]
-        count+=1
-    else:
-        if int(S)>641:
-            count+=2
-            break
-        else:
-            count+=1
-            break
+while len(S)>3:
+    i = 2
+    while S[i + 1] == '0' or int(S[:i+1])>641:
+        i -= 1
+    S = S[i + 1:]
+    count += 1
+
+if int(S) > 641:
+    count += 2
+
+else:
+    count += 1
 
 print(count)
