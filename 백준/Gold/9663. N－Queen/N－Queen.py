84748c9331482b754d34a7229954bf5661a0989d
DongSeok Kang <102869426+ehdtjr@users.noreply.github.com>
2024-03-17 17:04:08 +0900

    [Gold IV] Title: N-Queen, Time: 30300 ms, Memory: 208320 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -0,0 +1,29 @@
+import sys
+
+n=int(sys.stdin.readline())
+
+graph=[0]*n
+
+def attack(a,b):
+  for i in range(a):
+    if graph[i]==b or abs(a-i)==abs(graph[i]-b):
+      return False
+  return True
+
+cnt=0
+
+def dfs(x):
+  global cnt
+
+  if x==n:
+    cnt+=1
+
+  else:
+    for i in range(n):
+      if attack(x,i):
+        graph[x] = i
+        dfs(x+1)
+
+dfs(0)
+
+print(cnt)
