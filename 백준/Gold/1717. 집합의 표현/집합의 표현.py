fc62fef465bf8a716b7b0a0c4037f0dfb84a9b79
DongSeok Kang <102869426+ehdtjr@users.noreply.github.com>
2024-04-02 21:30:51 +0900

    [Gold V] Title: 집합의 표현, Time: 296 ms, Memory: 76584 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -0,0 +1,33 @@
+import sys
+sys.setrecursionlimit(10**8)
+
+v,e=map(int,sys.stdin.readline().split())
+
+parent=[i for i in range(v+1)]
+
+def find(i):
+    if parent[i]==i:
+        return parent[i]
+    else:
+        parent[i]=find(parent[i])
+        return parent[i]
+
+def union(a,b):
+    a=find(a)
+    b=find(b)
+
+    if a>b:
+        parent[a]=b
+    else:
+        parent[b]=a
+
+for i in range(e):
+    c,a,b=map(int,sys.stdin.readline().split())
+
+    if c==0:
+        union(a,b)
+    else:
+        if find(a)==find(b):
+            print('YES')
+        else:
+            print('NO')
